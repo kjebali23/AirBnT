4f9f228a46b330d61ca9ee44f52368808a5e4a98
kjebali23 <khalil.jebali.kj@gmail.com>
2022-08-03 16:45:06 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-center text-4xl text-blue-500">HI</h1>
+    <div className="btn">test</div>
+    </>
+  )
+}
+
+export default App
