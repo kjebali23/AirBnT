02b12d8e4e6ae549a3b6f7df7c715eaeb2c682ef
kjebali23 <khalil.jebali.kj@gmail.com>
2022-08-06 19:51:13 +0100

    Fixed footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer footer-center p-4 bg-slate-700 text-base-content">
   <div>
-    <p className="text-slate-100">Copyright © 2022 - All right reserved by Khalil jebali</p>
+    <p className="text-slate-100">Copyright © 2022 - All right reserved by <a className="underline decoration-solid text-white" href="https://github.com/kjebali23" target="_blank">Kjebali23</a></p>
   </div>
 </footer>
   )
