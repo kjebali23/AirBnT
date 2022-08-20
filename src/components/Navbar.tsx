963bcfca6e5a2fed16e20e93621537abae47609f
kjebali23 <khalil.jebali.kj@gmail.com>
2022-08-20 13:54:53 +0100

    Changed the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar  drop-shadow-sm flex justify-between ">
+    <div className="navbar  drop-shadow-sm flex justify-between  items-center">
         <div className="flex-2">
             <a className="btn btn-ghost normal-case text-3xl text-slate-700">Airb&t</a>
         </div>
-        <div className="flex-note gap-2">
-            <div className="form-control">
-                <input type="text" placeholder="Search" className="input input-bordered bg-white" />
-            </div>
+            <div className="nav-links mr-16">
+                <ul className="flex flex-row gap-6 font-medium text-lg">
+                  <li> <p className="hover:underline underline-offset-2">Home</p></li>
+                  <li><p className="hover:underline underline-offset-2">Plan a Trip</p></li>
+                  <li><p className="hover:underline underline-offset-2">About us</p></li>
+                </ul>
         </div>
     </div>
   )
